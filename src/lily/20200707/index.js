2b1d7544f16b83e7b4e832dcfc4fa67d4c1f69c6
Joson5 <wangsanmu5@163.com>
2020-08-20 18:35:10 +0800

    update lily

---
+++ src/lily/20200707/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Love extends Component {
+  state = {
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div class="piece-box">
+          <div class="piece-box2">
+            <div class="piece piece-1"></div>
+            <div class="piece piece-2"></div>
+            <div class="piece piece-3"></div>
+            <div class="piece piece-4"></div>
+            <div class="piece piece-5"></div>
+            <div class="piece piece-6"></div>
+          </div>
+          <div class="piece-box3">
+            <div class="piece piece-1"></div>
+            <div class="piece piece-2"></div>
+            <div class="piece piece-3"></div>
+            <div class="piece piece-4"></div>
+            <div class="piece piece-5"></div>
+            <div class="piece piece-6"></div>
+          </div>
+          <div class="piece-box4">
+            <div class="piece piece-1"></div>
+            <div class="piece piece-2"></div>
+            <div class="piece piece-3"></div>
+            <div class="piece piece-4"></div>
+            <div class="piece piece-5"></div>
+            <div class="piece piece-6"></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Love;
